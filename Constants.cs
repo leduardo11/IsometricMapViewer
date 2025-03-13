7330a00df4ae2f481cd3d95b7f26f18ccf3a04ea
Luis_Eduardo <undernet1@gmail.com>
2025-03-13 11:07:26 -0400

    .

--- Constants.cs
+++ Constants.cs
@@ -5,7 +5,7 @@ namespace IsometricMapViewer
     public static class Constants
     {
         public const string OutputPath = "/home/leduardo";
-        public const string MapName = "2ndmiddle";
+        public const string MapName = "promisedland";
         public const int TileWidth = 32;
         public const int TileHeight = 32;
         public const int ExpectedTileSize = 10;
